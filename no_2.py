6bb4331b412a8ab3eda3f176e26d1c0b7db0789f
ihsan3975 <m.ihsan3975@gmail.com>
2019-07-27 18:59:23 +0700

    Update no_2.py

--- no_2.py
+++ no_2.py
@@ -1,9 +1,9 @@
 import re
 
-regex_user = '(?=.*^[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{5,9}'
+regex_user = '^(?=.*^[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{5,9}$'
 username = input("input username:")
 print(bool(re.search(regex_user, username)))
 
-regex_pass = '(?=(?:.*[a-z]))(?=.*[$=_]{1})(?=.*[A-Z]{1})(?=.*[0-9]{1})(?=.*[$=_])[a-zA-Z0-9$=_$=_]{8,}'
+regex_pass = '^(?=(?:.*[a-z]))(?=.*[$=_]{1})(?=.*[A-Z]{1})(?=.*[0-9]{1})(?=.*[$=_])[a-zA-Z0-9$=_$=_]{8,}$'
 password = input("input password:")
 print(bool(re.search(regex_pass, password)))
